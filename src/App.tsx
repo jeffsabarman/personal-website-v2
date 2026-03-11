520b54ae26ee0f4a2cfeefb72456f384ce8c147f
Jeffrey Triandi <jeffsabarman@gmail.com>
2026-03-11 21:01:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+
+    return (
+        <section id="hero">
+            <div>JEFFREY HERO SECTION</div>
+        </section>
+    )
+}
+
+export default Hero
